4800bb7ed2c69abfe55319cb4fca7309a81a88ef
Murodjon <murodjon.tursunpulatov@gmail.com>
2021-04-19 16:06:14 +0500

    add total next operation props

--- src/components/Display.js
+++ src/components/Display.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Display = ({ total }) => <div className="display">{total || '0'}</div>;
+const Display = ({ total, next, operation }) => (
+  <div className="display">
+    {total || '0'}
+    {operation ? operation : ''}
+    {next}
+  </div>
+);
 
 Display.propTypes = {
   total: PropTypes.string,
+  next: PropTypes.string,
+  operation: PropTypes.string,
 };
 
 export default Display;
